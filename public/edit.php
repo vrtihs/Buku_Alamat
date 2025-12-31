5d8ed90185f87e2967d82e5e450c8a6f8144a956
gaputrisatya-hash <gaputrisatya@gmail.com>
2025-12-31 13:26:41 +0800

    Update edit.php

--- public/edit.php
+++ public/edit.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <title>Edit Kontak</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body class="bg-light">
 
